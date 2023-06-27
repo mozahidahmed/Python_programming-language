86fcc445839aa22166a6d57ecfba4d4605280043
mozahid ahmed <96901942+mozahidahmed@users.noreply.github.com>
2023-06-27 08:39:24 +0600

    Condition.py

---
+++ Condition.py
@@ -0,0 +1,8 @@
+#if elif else 
+number=int(input());
+print('you written number',number)
+
+if number >18:
+    print("now you are ready for married 🌸")
+    elif number<18:
+    print("you are notvready for mirred")
