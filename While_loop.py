02d5f6b5a79af0a9f02f3f4f87a6cdc1b526d680
mozahid ahmed <96901942+mozahidahmed@users.noreply.github.com>
2023-06-27 09:09:07 +0600

    While_loop.py

---
+++ While_loop.py
@@ -0,0 +1,20 @@
+#auto
+n = 3
+while  n<5:
+  print("chole chole chole")
+
+
+#off after 3 output
+n = 3
+i = 1
+while i <= n:
+  print("Loop is easy.")
+  i = i+1 
+
+
+#multplecation table
+n = 14
+i = 1
+while i <= 10:
+  print(n, 'x', i, '=', n*i)
+  i = i+1
