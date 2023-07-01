f1cedb1c8893e37428cfb512aed47c1b0b7472b7
mozahid ahmed <96901942+mozahidahmed@users.noreply.github.com>
2023-07-01 09:02:54 +0600

    Function.py

---
+++ Function.py
@@ -0,0 +1,33 @@
+#normal function 
+def name():
+  print("hello world)
+name ()
+
+#argument pass 
+def name(name):
+   print(name)
+name("mozahid)
+     
+#return function 
+     def name(name):
+     return name
+
+name=name("mozahid")
+print(name)
+
+
+#multple argument 
+   
+   def sum(n1,n2):
+   sum =n1+n2
+   return sum 
+
+   n1=10
+   n2=20 
+   result=sum(n1,n2)
+   print(result)
+
+   
+       
+  
+     
