4444dedf389f6f8fc92d26d5a3ed15774c0befd9
mozahid ahmed <96901942+mozahidahmed@users.noreply.github.com>
2023-07-01 13:13:46 +0600

    Local_gloval_variable.py

---
+++ Local_gloval_variable.py
@@ -0,0 +1,13 @@
+#local 
+def my_func():
+  x = 10
+  print("Value inside function:", x)
+my_func()
+print(x) #error 
+
+#gloval 
+def my_func():
+  x = 10
+  print("Value inside function:", x)
+my_func()
+print(x) #Error
