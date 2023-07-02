7d1f74452cda4d87b6a8b97b7b7970a250ff4143
mozahid ahmed <96901942+mozahidahmed@users.noreply.github.com>
2023-07-02 13:46:44 +0600

    Create Slice.py

---
+++ Slice.py
@@ -0,0 +1,11 @@
+print(my_tuple[1:4])  # ('y', 't', 'h')
+
+# elements from beginning to 2nd
+print(my_tuple[:-4])  # ('P', 'y')
+
+# elements 6th to end
+print(my_tuple[5:])   # ('n',)
+
+# elements from the beginning to the end
+print(my_tuple[:])
+# Output: ('P', 'y', 't', 'h', 'o', 'n')
