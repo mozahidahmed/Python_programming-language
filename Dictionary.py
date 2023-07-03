5ba573db10301b20f05383b3f6f99e854ee44769
mozahid ahmed <96901942+mozahidahmed@users.noreply.github.com>
2023-07-03 12:18:22 +0600

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,14 @@
+my_dict = dict({1:'apple', 2:'ball'})
+
+print(my_dict)
+# Output: {1: 'apple', 2: 'ball'}
+
+
+
+person = {'name': 'Jack', 'age': 26}
+
+person_name = person['name']
+print(person_name)  # Output: Jack
+
+person_age = person['age']
+print(person_age)  # Output: 26
